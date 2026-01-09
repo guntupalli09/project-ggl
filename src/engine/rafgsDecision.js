156e45a06250983039203e009d1243664f03c5fe
Santhosh Guntupalli <142756616+guntupalli09@users.noreply.github.com>
2026-01-09 17:43:20 -0600

    Create rafgsDecision.js with RAFGSRule constants

---
+++ src/engine/rafgsDecision.js
@@ -0,0 +1,7 @@
+// src/engine/rafgsDecision.js
+export const RAFGSRule = Object.freeze({
+  STATE_BLOCK: "STATE_BLOCK",
+  RESPONSE_SUPPRESSION: "RESPONSE_SUPPRESSION",
+  TIME_GATE: "TIME_GATE",
+  ELIGIBLE: "ELIGIBLE",
+});
